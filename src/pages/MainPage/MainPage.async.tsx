f856f131dd2c4b462aeff5991463218042abb111
Glorious99 <vlad.ponomariev.99@mail.ru>
2023-02-14 17:30:12 +0300

    initial commit

---
+++ src/pages/MainPage/MainPage.async.tsx
@@ -0,0 +1,13 @@
+import { lazy } from "react";
+
+//Рабочая модель
+// export const MainPageAsync = lazy(() => import("./MainPage"));
+
+// для проверки индикатора загрузки
+export const MainPageAsync = lazy(
+  () =>
+    new Promise((resolve) => {
+      //@ts-ignore
+      setTimeout(() => resolve(import("./MainPage")), 1500);
+    })
+);
