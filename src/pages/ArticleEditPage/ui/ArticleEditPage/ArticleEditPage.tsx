5e04e86e079c55c386c28f7841570b7ab746d042
Glorious99 <vlad.ponomariev.99@mail.ru>
2023-12-19 17:37:04 +0300

    less 61

--- src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.tsx
+++ src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.tsx
@@ -1,6 +1,5 @@
 import { memo } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
-import cls from "./ArticleEditPage.module.scss";
 import { Page } from "widgets/Page/Page";
 import { useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
@@ -16,7 +15,7 @@ const ArticleEditPage = memo((props: ArticleEditPageProps) => {
   const { t } = useTranslation("article-details");
 
   return (
-    <Page className={classNames(cls.ArticleEditPage, {}, [className])}>
+    <Page className={classNames("", {}, [className])}>
       {isEdit
         ? t("Редактирование статьи с ID") + id
         : t("Создание новой статьи")}
